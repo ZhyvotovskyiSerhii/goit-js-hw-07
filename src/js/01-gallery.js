e84a0cad97e6fc01aab8614d2d1448add99dbc16
ZhyvotovskyiSerhii <86567407+ZhyvotovskyiSerhii@users.noreply.github.com>
2021-08-31 22:23:23 +0300

    1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,53 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+
+
+const galleryImages = {
+    itemsList: document.querySelector('.gallery'),
+  };
+
+
+
+  function createGalleryImages(galleryItems) {
+    return galleryItems
+      .map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+       <li>
+        <a class="gallery__link" href="${original}">
+         <img
+           class="gallery__image"
+           src="${preview}"
+           data-source="${original}"
+           alt="${description}"
+         />
+        </a>
+       </li>
+     </div>`;
+      }).join('');
+  }
+  
+  galleryImages.itemsList.innerHTML = createGalleryImages(galleryItems);
+  galleryImages.itemsList.addEventListener('click', onImgClick);
+  
+  function onImgClick(event) {
+    event.preventDefault();
+
+    const modalWindow = {
+      onShow: () => {
+        document.addEventListener('keydown', keyEscPress);
+      },
+      onClose: () => {
+        document.removeEventListener('keydown', keyEscPress);
+      },
+    };
+    
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}">`, modalWindow);
+    instance.show();
+    function keyEscPress(event) {
+      if (event.key === 'Escape') {
+        instance.close();
+      }
+    }
+  }
+console.log(galleryItems);
